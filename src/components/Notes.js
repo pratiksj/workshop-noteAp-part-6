e53e7aebe48bb9b60f7416dbd2ba5e9b32985069
pratiksj <pratiksharai728@gmail.com>
2022-12-20 22:27:02 +0545

    moves filering logic to mapStateProps function

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,15 +4,15 @@ import { useDispatch } from "react-redux";
 
 const Notes = (props) => {
   const dispatch = useDispatch();
-  const notesToShow = () => {
-    if (props.filter === "ALL") {
-      return props.notes;
-    }
+  // const notesToShow = () => {
+  //   if (props.filter === "ALL") {
+  //     return props.notes;
+  //   }
 
-    return props.filter === "IMPORTANT"
-      ? props.notes.filter((note) => note.important)
-      : props.notes.filter((note) => !note.important);
-  };
+  //   return props.filter === "IMPORTANT"
+  //     ? props.notes.filter((note) => note.important)
+  //     : props.notes.filter((note) => !note.important);
+  // };
 
   // const notes = useSelector(({ filter, notes }) => {
   //   console.log(filter);
@@ -39,7 +39,7 @@ const Notes = (props) => {
             handleClick={() => dispatch(toggleImportanceOf(note.id))}
           />
         ))} */}
-        {notesToShow().map((note) => (
+        {props.notes.map((note) => (
           <li
             key={note.id}
             onClick={() => dispatch(toggleImportanceOf(note.id))}
@@ -53,9 +53,16 @@ const Notes = (props) => {
 };
 
 const mapStateToProps = (state) => {
+  if (state.filter === "ALL") {
+    return {
+      notes: state.notes,
+    };
+  }
   return {
-    notes: state.notes,
-    filter: state.filter,
+    notes:
+      state.filter === "IMPORTANT"
+        ? state.notes.filter((note) => note.important)
+        : state.notes.filter((note) => !note.important),
   };
 };
 const ConnectedNotes = connect(mapStateToProps)(Notes);
